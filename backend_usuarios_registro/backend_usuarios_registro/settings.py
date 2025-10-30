0ef468faebd776db38cf45018195b598e15feb78
MatiasFerreiraF <matiferreirafontes@gmail.com>
2025-10-30 11:59:02 -0300

    Update settings.py

--- backend_usuarios_registro/backend_usuarios_registro/settings.py
+++ backend_usuarios_registro/backend_usuarios_registro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$8rhj&1oiu#xhq_olzui8wmoa$^&vqebs!02c&zeb=9+^2u+d)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', "backend", "frontend", "notificaciones", "localhost"]
 
 
 # Application definition
