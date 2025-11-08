b6229b241815e667bf39440cc548f7df4abb33aa
Andyval31 <135876858+Andyval31@users.noreply.github.com>
2025-11-08 16:12:25 -0300

    Update urls.py

--- backend_usuarios_registro/backend_usuarios_registro/urls.py
+++ backend_usuarios_registro/backend_usuarios_registro/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HealthCheckView(APIView):
+        def get(self, request):
+                return Response({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/usuarios/', include('usuarios.urls')),
+    path("api/healthz/", HealthCheckView.as_view()),
 ]
+
+
